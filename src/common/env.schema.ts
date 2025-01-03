873fa9341d1029b0e3e99660b2dfae869ccbf5dd
Henry Mosquera <henrym.718@hotmail.com>
2025-01-03 18:55:11 +0100

    Realizar la conexion a la base de datos de desarrollo, y creacion de un esquema de prueba

--- src/common/env.schema.ts
+++ src/common/env.schema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const envSchema = z.object({
     PORT: z.string().min(1).transform(Number),
+    DATABASE_URL: z.string().url().min(1),
 });
 
 export type Env = z.infer<typeof envSchema>;
