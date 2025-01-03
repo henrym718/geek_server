b5e5f15c006eedcc1b0065dcfb6a0a1dfefa79ac
Henry Mosquera <henrym.718@hotmail.com>
2025-01-03 15:04:35 +0100

    Configirarcion de logger

---
+++ src/common/logger.ts
@@ -0,0 +1,20 @@
+import { createLogger, format, transports } from "winston";
+import pc from "picocolors";
+
+const levesl: Record<string, any> = {
+    error: pc.red,
+    warn: pc.yellow,
+    info: pc.green,
+    http: pc.cyan,
+};
+
+const formatLog = format.printf(({ level, message, timestamp }): string => {
+    const levelColor = levesl[level](`${level.toUpperCase()}:`);
+    return `${timestamp} ${levelColor} ${`[ ${message} ]`}`;
+});
+
+export const logger = createLogger({
+    level: "http",
+    format: format.combine(format.timestamp(), formatLog),
+    transports: [new transports.Console()],
+});
