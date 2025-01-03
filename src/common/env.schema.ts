9c1842996d7c4e5f60408baa822a7991767042f7
Henry Mosquera <henrym.718@hotmail.com>
2025-01-03 18:57:17 +0100

    Revert "Realizar la conexion a la base de datos de desarrollo, y creacion de un esquema de prueba"
    This reverts commit 873fa9341d1029b0e3e99660b2dfae869ccbf5dd.

--- src/common/env.schema.ts
+++ src/common/env.schema.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const envSchema = z.object({
     PORT: z.string().min(1).transform(Number),
-    DATABASE_URL: z.string().url().min(1),
 });
 
 export type Env = z.infer<typeof envSchema>;
--- src/init.bootstrap.ts
+++ src/init.bootstrap.ts
@@ -1,21 +1,16 @@
 import { ServerBootstrap } from "./bootsraps/server.bootsrap";
 import { EnvBootstrap } from "./bootsraps/env.bootstrap";
 import { logger } from "./common/logger";
-import { PrismaBootstrap } from "./bootsraps/prisma.bootsrap";
 
 const serverBootstrap = new ServerBootstrap();
 const envBootstrap = new EnvBootstrap();
-const prismaBootstrap = new PrismaBootstrap();
 
 (async () => {
     try {
         await envBootstrap.initialize();
-        await prismaBootstrap.inizialized();
         await serverBootstrap.initialize();
     } catch (error) {
-        logger.error(error);
-        await prismaBootstrap.close();
         await serverBootstrap.close();
-        process.exit(1);
+        logger.error(error);
     }
 })();
