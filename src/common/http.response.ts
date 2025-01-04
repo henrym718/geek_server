a721a8b13f125d43d74b1b2f81779593b8b3e303
Henry Mosquera <henrym.718@hotmail.com>
2025-01-04 19:37:09 +0100

    Adds HttpResponse class for handling success and failure responses
    This commit adds a new HttpResponse class that provides static methods for handling success and failure responses in the Express framework. The `success` method sets the response status to 200 and sends a JSON object with a `success` property set to true, along with optional `data` and `details` properties. The `failure` method allows setting a custom status code and sends a JSON object with a `success` property set to false, along with a `message` property. This class will help streamline response handling in the codebase.

---
+++ src/common/http.response.ts
@@ -0,0 +1,17 @@
+import { Response } from "express";
+
+export class HttpResponse {
+    public static success(res: Response, data: any, details?: string) {
+        res.status(200).json({
+            success: true,
+            data,
+            details,
+        });
+    }
+    public static failure(res: Response, status: number, message: string) {
+        res.status(status).json({
+            success: false,
+            message,
+        });
+    }
+}
