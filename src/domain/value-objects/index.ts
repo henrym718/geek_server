a5b98be97134fd5939f41086d82179f5edc4e459
Henry Mosquera <henrym.718@hotmail.com>
2025-01-10 16:29:31 +0100

    Agrega objetos de valor [IdVO, EmailVO, PasswordVO, ProviderVO, RoleVO, TokenVO] para entidades de dominio en un index.
    Esto facilita la importación de esos objetos de valor sin tener tantas importaciones de varias lineas.

---
+++ src/domain/value-objects/index.ts
@@ -0,0 +1,6 @@
+export { IdVO } from "./id.vo";
+export { EmailVO } from "./email.vo";
+export { PasswordVO } from "./password.vo";
+export { ProviderVO } from "./provider.vo";
+export { RoleVO } from "./role.vo";
+export { TokenVO } from "./token.dto";
