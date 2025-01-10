f0b91922aacd925f093c4a6792b4b665658c023d
Henry Mosquera <henrym.718@hotmail.com>
2025-01-10 18:00:10 +0100

    Agrega los objetos de valor RoleEnum y ProviderEnum
    Esta confirmación agrega los objetos de valor RoleEnum y ProviderEnum al directorio de objetos de valor del dominio. Estos objetos de valor se utilizan para representar roles y proveedores en el sistema.

--- src/domain/value-objects/index.ts
+++ src/domain/value-objects/index.ts
@@ -1,3 +1,5 @@
+export { RoleEnum } from "./role.vo";
+export { ProviderEnum } from "./provider.vo";
 export { IdVO } from "./id.vo";
 export { EmailVO } from "./email.vo";
 export { PasswordVO } from "./password.vo";
