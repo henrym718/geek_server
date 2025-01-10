12e88860ef07b463a3f434731a921ca200bdf1f7
Henry Mosquera <henrym.718@hotmail.com>
2025-01-10 16:30:46 +0100

    Refactoriza la clase User e introduce la interfaz UserCreateProps Actualiza la clase User para utilizar objetos de valor para las propiedades Introduce la interfaz UserCreateProps para crear nuevos usuarios Implementa la validación de contraseñas y tokenProvider en el método User.create

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,32 +1,63 @@
+import { HttpException } from "@Common/http.exception";
+import { EmailVO, IdVO, PasswordVO, ProviderVO, RoleVO, TokenVO } from "@Domain/value-objects";
+
+export interface UserCreateProps {
+    readonly id: IdVO;
+    readonly provider: ProviderVO;
+    readonly role: RoleVO;
+    readonly email: EmailVO;
+    readonly refreshToken: TokenVO;
+    readonly password?: PasswordVO;
+    readonly tokenProvider?: TokenVO;
+}
+
+export interface UserProps {
+    readonly id: IdVO;
+    readonly email: EmailVO;
+    readonly provider: ProviderVO;
+    readonly password: PasswordVO | null;
+    readonly tokenProvider: TokenVO | null;
+    readonly role: RoleVO;
+    readonly refreshToken: TokenVO;
+    readonly isActive: boolean;
+    readonly createdAt: Date;
+    readonly updatedAt: Date | null;
+}
+
 export class User {
     private constructor(private readonly props: UserProps) {}
 
-    static create(props: Omit<UserProps, "id" | "createdAt" | "updatedAt" | "isActive">): User {
-        return new User({ ...props, id: "idprovisional", createdAt: new Date(), updatedAt: new Date(), isActive: true });
+    static create(props: UserCreateProps): User {
+        if ((props.password && props.tokenProvider) || (!props.password && !props.tokenProvider)) {
+            throw HttpException.badRequest("Debe proporcionar password o tokenProvider, pero no ambos");
+        }
+        const now = new Date();
+        return new User({
+            ...props,
+            password: props.password || null,
+            tokenProvider: props.tokenProvider || null,
+            isActive: true,
+            createdAt: now,
+            updatedAt: null,
+        });
     }
 
     static reconstitute(props: UserProps): User {
         return new User(props);
     }
-}
 
-export interface UserProps {
-    readonly id: string;
-    readonly email: string;
-    readonly password: string | null;
-    readonly provider: AuthProviderEnum;
-    readonly role: RoleEnum;
-    readonly isActive: boolean;
-    readonly createdAt: Date;
-    readonly updatedAt: Date;
-}
-
-export enum AuthProviderEnum {
-    LOCAL = "LOCAL",
-    GOOGLE = "GOOGLE",
-}
-
-export enum RoleEnum {
-    CLIENT = "CLIENT",
-    VENDOR = "VENDOR",
+    public toJSON() {
+        return {
+            id: this.props.id.getValue(),
+            email: this.props.email.getValue(),
+            provider: this.props.provider.getValue(),
+            password: this.props.password ? this.props.password.getValue() : null,
+            tokenProvider: this.props.tokenProvider ? this.props.tokenProvider.getValue() : null,
+            role: this.props.role.getValue(),
+            refreshToken: this.props.refreshToken.getValue(),
+            isActive: this.props.isActive,
+            createdAt: this.props.createdAt,
+            updatedAt: this.props.updatedAt,
+        };
+    }
 }
