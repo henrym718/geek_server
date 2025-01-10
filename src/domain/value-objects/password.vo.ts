ac4c1cc32b972308941ba243f3b4db3f038f8045
Henry Mosquera <henrym.718@hotmail.com>
2025-01-10 14:54:51 +0100

    Añade el objeto de valor PasswordVO para el manejo de contraseñas
    Esta confirmación introduce el objeto de valor PasswordVO, que proporciona métodos para crear objetos de contraseña a partir de texto sin formato o valores codificados. También incluye la validación de la solidez de la contraseña. Esta incorporación mejora la organización del código y mejora la seguridad al encapsular la lógica relacionada con las contraseñas.

---
+++ src/domain/value-objects/password.vo.ts
@@ -0,0 +1,46 @@
+import { HttpException } from "@Common/http.exception";
+
+export class PasswordVO {
+    private constructor(private readonly value: string, private readonly isHashed: boolean) {}
+
+    public static fromPlainText(password: string): PasswordVO {
+        this.validatePlainText(password);
+        return new PasswordVO(password, false);
+    }
+
+    public static fromHash(hash: string): PasswordVO {
+        this.validateHash(hash);
+        return new PasswordVO(hash, true);
+    }
+
+    public equals(other: PasswordVO): boolean {
+        if (this.isHashed !== other.isHashed) {
+            return false;
+        }
+        return this.value === other.value;
+    }
+
+    public isHashedPassword(): boolean {
+        return this.isHashed;
+    }
+
+    public getValue(): string {
+        return this.value;
+    }
+
+    private static validatePlainText(password: string): void {
+        if (!password) {
+            throw HttpException.badRequest("Password is required");
+        }
+
+        if (!new RegExp(/^(?=.*\d)(?=.*[a-zA-Z]).{5,}$/).test(password)) {
+            throw HttpException.badRequest("Invalid password from VO");
+        }
+    }
+
+    private static validateHash(hash: string): void {
+        if (!hash) {
+            throw HttpException.badRequest("Hash is required");
+        }
+    }
+}
