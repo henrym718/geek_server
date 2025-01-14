b16fa7991162891762ccc428e581e933635fb621
Henry Mosquera <henrym.718@hotmail.com>
2025-01-14 16:41:21 +0100

    Agrega variables de entorno para el puerto de servicio, la URL de la base de datos y los tokens de autenticación
    Esta confirmación agrega variables de entorno para el puerto de servicio, la URL de la base de datos y los tokens de autenticación. La variable `PORT` se utiliza para especificar el puerto en el que se ejecutará el servicio. La variable `DATABASE_URL` se utiliza para almacenar la URL de la base de datos. La variable `KEY_SECRET_TOKEN` se utiliza para almacenar la clave secreta para la generación de tokens. Además, las variables `ACCESS_TOKEN_EXPIRATION` y `REFRESH_TOKEN_EXPIRATION` se utilizan para especificar el tiempo de vencimiento de los tokens de acceso y actualización, respectivamente.

--- src/common/env.schema.ts
+++ src/common/env.schema.ts
@@ -1,8 +1,16 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
+    // Servicdor
     PORT: z.string().min(1).transform(Number),
+
+    // Base de datos
     DATABASE_URL: z.string().url().min(1),
+
+    // Autenticación
+    KEY_SECRET_TOKEN: z.string().min(32),
+    ACCESS_TOKEN_EXPIRATION: z.string(),
+    REFRESH_TOKEN_EXPIRATION: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
