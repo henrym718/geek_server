4d9194835a34af4122caad372e8bd38699b2bc52
Henry Mosquera <henrym.718@hotmail.com>
2025-02-20 11:26:54 -0500

    Agrega interfaces DTO de creación de proveedores
    Incluye las interfaces ReqCreateVendorDto y ResCreateVendorDto
    Define la interfaz VendorProfile para la información del perfil del proveedor

--- src/modules/vendor/application/use-cases/create-vendor/create-vendor.dto.ts
+++ src/modules/vendor/application/use-cases/create-vendor/create-vendor.dto.ts
@@ -1,2 +1,29 @@
+import { RoleEnum } from "@Core/value-objects";
 
+export interface ReqCreateVendorDto {
+    id: string;
+    firtName: string;
+    lastName: string;
+    photo: string;
+    phone: string;
+    city: string;
+}
 
+export interface ResCreateVendorDto {
+    id: string;
+    email: string;
+    role: RoleEnum.VENDOR;
+    profileCompleted: boolean;
+    createdAt: Date;
+    updatedAt: Date | null;
+    isActive: boolean;
+    vendorProfile: VendorProfile;
+}
+
+interface VendorProfile {
+    firstName: string;
+    lastName: string;
+    photo?: string;
+    phone: string;
+    city: string;
+}
