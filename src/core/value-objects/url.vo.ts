83fae3930380be2579852145287c62d5e6a66c09
Henry Mosquera <henrym.718@hotmail.com>
2025-02-20 21:10:14 -0500

    Refactoriza la validación de URL en UrlVO.create
    Actualiza el patrón S3_REGEX para incluir compatibilidad con extensiones de archivos de imagen adicionales. Esto mejora la validación de URL para URL de S3 al permitir URL que terminan con extensiones de archivo .jpg, .jpeg, .png, .gif, .bmp o .tiff.

--- src/core/value-objects/url.vo.ts
+++ src/core/value-objects/url.vo.ts
@@ -2,11 +2,12 @@ import { HttpException } from "@Common/exceptions/http.exception";
 
 export class UrlVO {
     private static readonly STANDARD_REGEX = /^https?:\/\/(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\/[^\s]*)?$/;
-    private static readonly S3_REGEX = /^https?:\/\/(?:[a-zA-Z0-9-]+\.)?s3\.[a-zA-Z0-9-]+\.amazonaws\.com\/[^\s]*$/;
+    private static readonly S3_REGEX =
+        /^https?:\/\/([a-zA-Z0-9-]+)\.s3(\.[a-zA-Z0-9-]+)?\.amazonaws\.com\/([a-zA-Z0-9\-\/]+)\.(jpg|jpeg|png|gif|bmp|tiff)$/;
 
     private constructor(private readonly url: string) {}
 
-    public static create(url: string, type: "standard" | "s3" = "standard"): UrlVO {
+    public static create(url: string, type: "standard" | "s3"): UrlVO {
         this.validateUrl(type, url);
         return new UrlVO(url);
     }
