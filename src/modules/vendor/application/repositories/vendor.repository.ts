32b619333a12d3746722f44d5081d0eca154d7b8
Henry Mosquera <henrym.718@hotmail.com>
2025-02-20 21:11:09 -0500

    Implements findVendorByidWithUser method in IVendorRepository
    Adds a new method, findVendorByidWithUser, to the IVendorRepository interface. This method allows retrieving a vendor and its associated user by ID. TImplementa el método findVendorByidWithUser en IVendorRepository
    
    Agrega un nuevo método, findVendorByidWithUser, a la interfaz IVendorRepository. Este método permite recuperar un proveedor y su usuario asociado por ID. Este cambio mejora la funcionalidad del repositorio de proveedores al proporcionar una forma conveniente de recuperar datos de proveedores junto con los datos de usuario correspondientes.his change enhances the functionality of the vendor repository by providing a convenient way to retrieve vendor data along with the corresponding user data.

--- src/modules/vendor/application/repositories/vendor.repository.ts
+++ src/modules/vendor/application/repositories/vendor.repository.ts
@@ -1,4 +1,7 @@
+import { User } from "@Core/entities/user";
 import { Vendor } from "@Core/entities/vendor";
 import { IRepository } from "@Shared/repositories/repository";
 
-export interface IVendorRepository extends IRepository<Vendor> {}
+export interface IVendorRepository extends IRepository<Vendor> {
+    findVendorByidWithUser(id: string): Promise<{ user: User; vendor: Vendor } | null>;
+}
