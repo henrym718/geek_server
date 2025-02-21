04178f45df5d8728517b74c5bc7476b037586084
Henry Mosquera <henrym.718@hotmail.com>
2025-02-20 21:13:36 -0500

    Refactoriza UserPrismaRepository para utilizar mapeadores para la transformación de datos
    Actualiza UserPrismaRepository para utilizar UserMapper y VendorMapper para la transformación de datos
    Elimina importaciones innecesarias y funciones no utilizadas

--- src/modules/user/infrastructure/persistence/user-prisma.repository.ts
+++ src/modules/user/infrastructure/persistence/user-prisma.repository.ts
@@ -1,11 +1,11 @@
 import { PrismaBootstrap } from "@Bootstraps/prisma.bootsrap";
-import { EmailVO, IdVO, PasswordVO, ProviderVO, RoleVO, TextVO, TokenVO, UrlVO } from "@Core/value-objects";
+import { IdVO, TextVO } from "@Core/value-objects";
 import { IUserRepository } from "@User/application/ports/user.repository";
 import { User } from "@Core/entities/user";
-import { Prisma, User as UserPrisma } from "@prisma/client";
 import { Client } from "@Core/entities/client";
 import { Vendor } from "@Core/entities/vendor";
-import { PhoneVO } from "@Core/value-objects/phone.vo";
+import { VendorMapper } from "@Vendor/infraestructure/persistence/vendor.mapper";
+import { UserMapper } from "./user.mapper";
 
 export class UserPrismaRepository implements IUserRepository {
     private get prisma() {
@@ -13,9 +13,7 @@ export class UserPrismaRepository implements IUserRepository {
     }
 
     async create(user: User): Promise<void> {
-        await this.prisma.user.create({
-            data: this.toPrisma(user),
-        });
+        await this.prisma.user.create({ data: UserMapper.toPrisma(user) });
     }
 
     findById(id: string): Promise<User | null> {
@@ -28,8 +26,8 @@ export class UserPrismaRepository implements IUserRepository {
         if (!userFound) return null;
         const { client, vendor, ...userData } = userFound;
         return {
-            user: this.toDomain(userData),
-            vendor: vendor ? this.toVendorDomain(vendor) : null,
+            user: UserMapper.toDomain(userData),
+            vendor: vendor ? VendorMapper.toDomain(vendor) : null,
             client: client ? this.toClientDomain(client) : null,
         };
     }
@@ -41,53 +39,14 @@ export class UserPrismaRepository implements IUserRepository {
     async update(user: User): Promise<void> {
         await this.prisma.user.update({
             where: { id: user.id.getValue() },
-            data: this.toPrisma(user),
+            data: UserMapper.toPrisma(user),
         });
     }
 
     async findbyEmail(email: string): Promise<User | null> {
         const userFound = await this.prisma.user.findUnique({ where: { email } });
         if (!userFound) return null;
-        return this.toDomain(userFound);
-    }
-
-    private toPrisma(user: User): Prisma.UserCreateInput {
-        return {
-            id: user.id.getValue(),
-            provider: user.provider.getValue(),
-            email: user.email.getValue(),
-            password: user.password?.getValue() ?? null,
-            role: user.role.getValue(),
-            refresToken: user.refreshToken.getValue(),
-            tokenProvider: user.tokenProvider?.getValue() ?? null,
-        };
-    }
-
-    private toDomain(user: UserPrisma): User {
-        return User.reconstitute({
-            id: IdVO.create(user.id),
-            provider: ProviderVO.fromPlainText(user.provider),
-            email: EmailVO.create(user.email),
-            password: user.password ? PasswordVO.fromPlainText(user.password) : null,
-            role: RoleVO.fromPlainText(user.role),
-            isActive: user.isActive,
-            refreshToken: TokenVO.create(user.refresToken),
-            tokenProvider: user.tokenProvider ? TokenVO.create(user.tokenProvider) : null,
-            createdAt: user.createdAt,
-            updatedAt: user.updatedAt,
-        });
-    }
-
-    private toVendorDomain(vendor: any): Vendor {
-        return Vendor.reconstitute({
-            id: IdVO.create(vendor.id),
-            firstName: TextVO.create("firstName", vendor.firstName),
-            lastName: TextVO.create("lastName", vendor.lastName),
-            photo: UrlVO.create("photo", vendor.photo),
-            phone: PhoneVO.create(vendor.phone),
-            city: TextVO.create("city", vendor.city),
-            userId: IdVO.create(vendor.userId),
-        });
+        return UserMapper.toDomain(userFound);
     }
 
     private toClientDomain(client: any): Client {
