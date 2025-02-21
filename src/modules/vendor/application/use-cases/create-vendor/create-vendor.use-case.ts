2db689df863459679d36f69c770659872eb986ac
Henry Mosquera <henrym.718@hotmail.com>
2025-02-20 21:12:29 -0500

    Refactorizar el nombre del parámetro del método createVendor para mayor claridad
    Actualizar el nombre del parámetro de 'req' a 'data' en el método 'execute' de la interfaz 'ICreateVendorUseCase' para mejorar la legibilidad y claridad del código.

--- src/modules/vendor/application/use-cases/create-vendor/create-vendor.use-case.ts
+++ src/modules/vendor/application/use-cases/create-vendor/create-vendor.use-case.ts
@@ -1,5 +1,5 @@
 import { ReqCreateVendorDto, ResCreateVendorDto } from "./create-vendor.dto";
 
 export interface ICreateVendorUseCase {
-    execute(req: ReqCreateVendorDto): Promise<ResCreateVendorDto>;
+    execute(data: ReqCreateVendorDto): Promise<ResCreateVendorDto>;
 }
