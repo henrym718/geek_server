a773c769988dc0d17a8dcf2a931c9cff054a72c9
Henry Mosquera <henrym.718@hotmail.com>
2025-02-26 18:37:07 -0500

    Agrega un punto final de creación de habilidades
    Implementa el punto final de creación de habilidades en el módulo API de habilidades

---
+++ src/modules/skill/presentation/skill.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { skillContainer } from "../infraestructure/container/skill.container";
+import { SkillController } from "./skill.controller";
+
+export const skillRoutes = Router();
+const skillController = skillContainer.get(SkillController);
+
+skillRoutes.post("/", skillController.create.bind(skillController));
