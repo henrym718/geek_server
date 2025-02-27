97c0d05ecd029b415ad086d929e07e8468319245
Henry Mosquera <henrym.718@hotmail.com>
2025-02-26 22:24:07 -0500

    Refactoriza los métodos create y findById de SkillPrismaRepository
    Actualiza el método create de SkillPrismaRepository para usar async/await y SkillMapper para la persistencia de datos. Actualiza el método findById para devolver una Promesa de tipo Skill en lugar de un objeto con propiedades explícitas.

--- src/modules/skill/infraestructure/persistence/skill-prisma.repository.ts
+++ src/modules/skill/infraestructure/persistence/skill-prisma.repository.ts
@@ -1,24 +1,26 @@
 import { PrismaBootstrap } from "@Bootstraps/prisma.bootsrap";
+import { Skill } from "@Core/entities/skill";
 import { ISkillRepository } from "modules/skill/application/repositories/skill.repository";
+import { SkillMapper } from "./skill.mapper";
 
 export class SkillPrismaRepository implements ISkillRepository {
     private get prisma() {
         return PrismaBootstrap.prisma;
     }
 
-    create(data: { name: string; id: string; categoryId: string }): Promise<void> {
+    async create(data: Skill): Promise<void> {
+        await this.prisma.skill.create({ data: SkillMapper.toPersistence(data) });
+    }
+
+    update(entity: Skill): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
-    update(entity: { name: string; id: string; categoryId: string }): Promise<void> {
+    findById(id: string): Promise<Skill | null> {
         throw new Error("Method not implemented.");
     }
 
-    findById(id: string): Promise<{ name: string; id: string; categoryId: string } | null> {
-        throw new Error("Method not implemented.");
-    }
-
-    findAll(): Promise<{ name: string; id: string; categoryId: string }[]> {
+    findAll(): Promise<Skill[]> {
         throw new Error("Method not implemented.");
     }
 }
