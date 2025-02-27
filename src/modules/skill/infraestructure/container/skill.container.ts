0f17f680be2a104dd16654a62b4fd30ba0ac4b8c
Henry Mosquera <henrym.718@hotmail.com>
2025-02-26 22:24:26 -0500

    Agrega contenedores compartidos y de categorías a la jerarquía de contenedores de habilidades
    Actualiza el contenedor de habilidades para incluir contenedores compartidos y de categorías como elementos principales

--- src/modules/skill/infraestructure/container/skill.container.ts
+++ src/modules/skill/infraestructure/container/skill.container.ts
@@ -5,8 +5,13 @@ import { SkillPrismaRepository } from "../persistence/skill-prisma.repository";
 import { ICreateSkillUseCase } from "modules/skill/application/use-case/create/create-skill.use-case";
 import { CreateSkillUseCase } from "modules/skill/application/use-case/create/create-skill.impl";
 import { SkillController } from "modules/skill/presentation/skill.controller";
+import { sharedContainer } from "@Shared/container/shared.container";
+import { categoryContainer } from "@Category/infraestructure/container/category.container";
 
 export const skillContainer = new Container();
+
+skillContainer.parent = sharedContainer;
+sharedContainer.parent = categoryContainer;
 
 skillContainer.bind<ISkillRepository>(SKILL_SYMBOLS.SkillRepository).to(SkillPrismaRepository);
 skillContainer.bind<ICreateSkillUseCase>(SKILL_SYMBOLS.CreateSkillUseCase).to(CreateSkillUseCase);
