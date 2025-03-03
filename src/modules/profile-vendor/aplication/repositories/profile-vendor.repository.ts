eb6cdceb97b359139e416847e46781d9e0699325
Henry Mosquera <henrym.718@hotmail.com>
2025-03-03 10:20:53 -0500

    Añade la interfaz del repositorio de proveedores de perfiles
    Esta confirmación añade la interfaz del repositorio de proveedores de perfiles, que extiende la interfaz del repositorio genérico. Esto permitirá operaciones CRUD específicas para la entidad de proveedores de perfiles.

---
+++ src/modules/profile-vendor/aplication/repositories/profile-vendor.repository.ts
@@ -0,0 +1,4 @@
+import { ProfileVendor } from "@Core/entities/profile-vendor";
+import { IRepository } from "@Shared/repositories/repository";
+
+export interface IProfileVendorRepository extends IRepository<ProfileVendor> {}
