0f1f997b9d6c646717dbd59aaca35977fcc9b77f
Henry Mosquera <henrym.718@hotmail.com>
2025-03-03 10:18:56 -0500

    Agrega el método findByIds a ISkillRepository
    Esta confirmación agrega un nuevo método, findByIds, a la interfaz ISkillRepository. El nuevo método permite recuperar habilidades por sus identificadores. Este cambio mejora la flexibilidad para recuperar habilidades específicas según sus identificadores.

--- src/modules/skill/application/repositories/skill.repository.ts
+++ src/modules/skill/application/repositories/skill.repository.ts
@@ -1,4 +1,6 @@
 import { Skill } from "@Core/entities/skill";
 import { IRepository } from "@Shared/repositories/repository";
 
-export interface ISkillRepository extends IRepository<Skill> {}
+export interface ISkillRepository extends IRepository<Skill> {
+    findByIds(ids: string[]): Promise<Skill[]>;
+}
