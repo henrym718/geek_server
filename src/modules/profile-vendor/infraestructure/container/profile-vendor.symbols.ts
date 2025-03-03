092ef68398d3663302920b3d03b0e8c221091eb3
Henry Mosquera <henrym.718@hotmail.com>
2025-03-03 10:22:41 -0500

    Agrega símbolos de proveedores de perfiles para la inyección de dependencias
    Presenta el objeto PROFILE_VENDOR_SYMBOLS, que define símbolos para el repositorio de proveedores de perfiles y crea un caso de uso de proveedores de perfiles en el contenedor de inyección de dependencias.

---
+++ src/modules/profile-vendor/infraestructure/container/profile-vendor.symbols.ts
@@ -0,0 +1,4 @@
+export const PROFILE_VENDOR_SYMBOLS = {
+    profileVendorRepository: Symbol.for("ProfileVendorRepository"),
+    createProfileVendorUseCase: Symbol.for("CreateProfileVendorUseCase"),
+};
