c1445694ca1ac37ba08adffbf00bed1b0f860c3e
Henry Mosquera <henrym.718@hotmail.com>
2025-03-03 19:49:51 -0500

    Refactoriza la declaración de importación en create-profile-vendor.use-case.ts
    Actualiza la ruta de importación del archivo create-profile-vendor.dto para que sea relativa

--- src/modules/profile-vendor/aplication/use-cases/create-profile-vendor.use-case.ts
+++ src/modules/profile-vendor/aplication/use-cases/create-profile-vendor.use-case.ts
@@ -1,4 +1,4 @@
-import { ReqCreateProfileVendorDto, ResCreateProfileVendorDto } from "@ProfileVendor/aplication/use-cases/create-profile-vendor.dto";
+import { ReqCreateProfileVendorDto, ResCreateProfileVendorDto } from "./create-profile-vendor.dto";
 
 export interface ICreateProfileVendorUseCase {
     execute(data: ReqCreateProfileVendorDto): Promise<ResCreateProfileVendorDto>;
