30a5f2f940737dc718f543f166f16fd2033d0a35
Henry Mosquera <henrym.718@hotmail.com>
2025-03-03 19:54:41 -0500

    Implementa la función de autenticación de usuarios
    Agrega middleware de autenticación y verificación de roles
    Actualiza el punto final de creación de clientes para incluir autenticación y verificación de roles

--- src/modules/client/presentation/client.routes.ts
+++ src/modules/client/presentation/client.routes.ts
@@ -1,8 +1,11 @@
 import { clientContainer } from "@Client/infraestructure/container/clinet.container";
 import { Router } from "express";
 import { ClientController } from "./client.controller";
+import { authenticate } from "@Common/middlewares/authenticate";
+import { checkRoles } from "@Common/middlewares/checkRoles";
+import { RoleEnum } from "@Core/value-objects";
 
 export const clientRoutes = Router();
 const clientController = clientContainer.get(ClientController);
 
-clientRoutes.post("/create", clientController.createClient);
+clientRoutes.post("/create", authenticate, checkRoles(RoleEnum.CLIENT), clientController.createClient);
