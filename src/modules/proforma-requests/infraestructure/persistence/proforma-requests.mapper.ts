c8168913578f80440a359d97bf6a0bd8fb39fca5
Henry Mosquera <henrym.718@hotmail.com>
2025-03-03 19:49:06 -0500

    Agrega ProformaRequestsMapper para operaciones de persistencia
    Esta confirmación agrega la clase ProformaRequestsMapper, que proporciona métodos para convertir entre entidades de dominio y entidades de persistencia en el módulo Proforma Requests. El método `toPersistence` convierte un objeto ProformaRequest en un objeto Prisma.ProformaRequestCreateInput, mientras que el método `toDomain` convierte un objeto PrismaProformaRequest en un objeto ProformaRequest. Estos métodos de mapeo son esenciales para manejar el almacenamiento y la recuperación de datos en el módulo.

---
+++ src/modules/proforma-requests/infraestructure/persistence/proforma-requests.mapper.ts
@@ -0,0 +1,31 @@
+import { ProformaRequest } from "@Core/entities/proforma-requests";
+import { IdVO, PriceVO, StatusVO, TextVO } from "@Core/value-objects";
+import { Prisma, ProformaRequest as PrismaProformaRequest } from "@prisma/client";
+
+export class ProformaRequestsMapper {
+    public static toPersistence(entity: ProformaRequest): Prisma.ProformaRequestCreateInput {
+        return {
+            id: entity.id.getValue(),
+            description: entity.description.getValue(),
+            budget: entity.budget.getValue(),
+            status: entity.status.getValue(),
+            category: { connect: { id: entity.categoryId.getValue() } },
+            client: { connect: { id: entity.clientId.getValue() } },
+            skills: { connect: entity.skills.map((skillId) => ({ id: skillId.getValue() })) },
+        };
+    }
+
+    public static toDomain(entity: PrismaProformaRequest): ProformaRequest {
+        return ProformaRequest.reconstitute({
+            id: IdVO.create(entity.id),
+            description: TextVO.create("description", entity.description),
+            budget: PriceVO.create(entity.budget),
+            status: StatusVO.fromPlainText(entity.status),
+            categoryId: IdVO.create(entity.categoryId),
+            clientId: IdVO.create(entity.clientId),
+            skills: [],
+            createdAt: entity.createdAt,
+            updatedAt: entity.updatedAt,
+        });
+    }
+}
