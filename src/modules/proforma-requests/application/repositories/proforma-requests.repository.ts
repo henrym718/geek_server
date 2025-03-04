43a6001e848c2969becf36730e98d71166efd939
Henry Mosquera <henrym.718@hotmail.com>
2025-03-03 19:48:18 -0500

    Agrega la interfaz ProformaRequestsRepository
    Introduce una nueva interfaz para ProformaRequestsRepository, que extiende la interfaz IRepository para gestionar entidades ProformaRequest.

---
+++ src/modules/proforma-requests/application/repositories/proforma-requests.repository.ts
@@ -0,0 +1,4 @@
+import { ProformaRequest } from "@Core/entities/proforma-requests";
+import { IRepository } from "@Shared/repositories/repository";
+
+export interface IProformaRequestsRepository extends IRepository<ProformaRequest> {}
