b1ee3d56f80ac2280954769ef71a7d725464fc3e
Henry Mosquera <henrym.718@hotmail.com>
2025-03-03 19:53:09 -0500

    Añade puntos finales de API de solicitudes proforma
    Implementa los puntos finales de API para crear solicitudes proforma. Añade la ruta POST "/proforma-requests" con autenticación y control de acceso basado en roles. También incluye las dependencias y el controlador necesarios para gestionar las solicitudes.

---
+++ src/modules/proforma-requests/presnetation/proforma-requests.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { proformaRequestsContainer } from "@ProformaRequests/infraestructure/container/proforma-requests.container";
+import { ProformaRequestsController } from "./proforma-requests.controller";
+import { authenticate } from "@Common/middlewares/authenticate";
+import { checkRoles } from "@Common/middlewares/checkRoles";
+import { RoleEnum } from "@Core/value-objects";
+
+export const proformarequestRoutes = Router();
+const proformaRequestController = proformaRequestsContainer.get(ProformaRequestsController);
+
+proformarequestRoutes.post("/", authenticate, checkRoles(RoleEnum.CLIENT), proformaRequestController.create);
