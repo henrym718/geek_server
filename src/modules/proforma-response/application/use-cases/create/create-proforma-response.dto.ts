00681c412690f89eb8da7934dfccb31b5c13ad30
Henry Mosquera <henrym.718@hotmail.com>
2025-03-05 21:18:03 -0500

    Agrega interfaces DTO create-proforma-response
    Esta confirmación agrega las interfaces `ReqCreateProformaResponseDto` y `ResCreateProformaResponseDto` al archivo `create-proforma-response.dto.ts`. Estas interfaces definen la estructura de los objetos de solicitud y respuesta utilizados en la función de creación de respuesta proforma.

---
+++ src/modules/proforma-response/application/use-cases/create/create-proforma-response.dto.ts
@@ -0,0 +1,10 @@
+export interface ReqCreateProformaResponseDto {
+    budget: number;
+    message: string;
+    profileVendorId: string;
+    proformaRequestId: string;
+}
+
+export interface ResCreateProformaResponseDto {
+    details: string;
+}
