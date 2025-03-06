9b2309a44192cae92913ba7eed8a80cea8c35f0b
Henry Mosquera <henrym.718@hotmail.com>
2025-03-05 21:16:47 -0500

    Actualización de la declaración de importación en ProformaRequestsController
    Corrige un error tipográfico en la declaración de importación de ProformaRequestsController, lo que corrige la importación de PROFORMA_REQUEST_SYMBOLS a PROFORMA_REQ_SYMBOLS.
    
    Esta confirmación garantiza que se utilice la importación correcta, lo que evita posibles problemas relacionados con la declaración de importación.

--- src/modules/proforma-requests/presnetation/proforma-requests.controller.ts
+++ src/modules/proforma-requests/presnetation/proforma-requests.controller.ts
@@ -1,14 +1,14 @@
 import { HttpResponse } from "@Common/response/http.response";
 import { ReqCreateProformaRequestDto } from "@ProformaRequests/application/use-cases/create-proforma-requests/create-proforma-requests.dto";
 import { ICreateProformaRequestsUseCase } from "@ProformaRequests/application/use-cases/create-proforma-requests/create-proforma-requests.use-case";
-import { PROFORMA_REQUEST_SYMBOLS } from "@ProformaRequests/infraestructure/container/proforma-requests.symbols";
+import { PROFORMA_REQ_SYMBOLS } from "@ProformaRequests/infraestructure/container/proforma-requests.symbols";
 import { NextFunction, Request, Response } from "express";
 import { inject, injectable } from "inversify";
 
 @injectable()
 export class ProformaRequestsController {
     constructor(
-        @inject(PROFORMA_REQUEST_SYMBOLS.CreateProformaRequestsUseCase) private readonly createproformaRequestUseCase: ICreateProformaRequestsUseCase
+        @inject(PROFORMA_REQ_SYMBOLS.CreateProformaRequestsUseCase) private readonly createproformaRequestUseCase: ICreateProformaRequestsUseCase
     ) {
         this.create = this.create.bind(this);
     }
