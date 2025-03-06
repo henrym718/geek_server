8484f5b10e8f9fbbbc8a7f4a073af601aa414460
Henry Mosquera <henrym.718@hotmail.com>
2025-03-05 21:19:22 -0500

    Agrega ProformaResponsePrismaRepository para persistencia
    Implementa la clase ProformaResponsePrismaRepository, que proporciona métodos para crear, actualizar y recuperar entidades ProformaResponse de la base de datos mediante Prisma. Esto permite una integración perfecta del módulo ProformaResponse con la capa de persistencia.

---
+++ src/modules/proforma-response/infraestructure/persistence/proforma-reponse-prisma.repository.ts
@@ -0,0 +1,29 @@
+import { PrismaBootstrap } from "@Bootstraps/prisma.bootsrap";
+import { ProformaResponse } from "@Core/entities/proforma-response";
+import { IProformaResponseRepository } from "modules/proforma-response/application/repositories/proforma-response.repository";
+import { ProformaReponseMapper } from "./proforma-reponse.mapper";
+
+export class ProformaResponsePrismaRepository implements IProformaResponseRepository {
+    private get db() {
+        return PrismaBootstrap.prisma.proformaResponse;
+    }
+
+    async create(data: ProformaResponse): Promise<void> {
+        console.log("vendor", data.profileVendorId);
+        console.log("proforma-response", data.proformaRequestId);
+
+        await this.db.create({ data: ProformaReponseMapper.toPersistence(data) });
+    }
+
+    update(entity: ProformaResponse): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    findById(id: string): Promise<ProformaResponse | null> {
+        throw new Error("Method not implemented.");
+    }
+
+    findAll(): Promise<ProformaResponse[]> {
+        throw new Error("Method not implemented.");
+    }
+}
