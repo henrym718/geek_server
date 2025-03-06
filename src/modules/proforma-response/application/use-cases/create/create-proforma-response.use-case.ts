3ca4da7b25452628a5e44e99b9a9fd1429535afe
Henry Mosquera <henrym.718@hotmail.com>
2025-03-05 21:18:44 -0500

    Agrega la interfaz de caso de uso create-proforma-response
    Define la interfaz ICreateProformaResponseUseCase para crear una respuesta proforma

---
+++ src/modules/proforma-response/application/use-cases/create/create-proforma-response.use-case.ts
@@ -0,0 +1,5 @@
+import { ReqCreateProformaResponseDto, ResCreateProformaResponseDto } from "./create-proforma-response.dto";
+
+export interface ICreateProformaResponseUseCase {
+    execute(data: ReqCreateProformaResponseDto): Promise<ResCreateProformaResponseDto>;
+}
