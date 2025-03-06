684dce3572997f01381d2ea1351e546dec5ae391
Henry Mosquera <henrym.718@hotmail.com>
2025-03-05 21:18:26 -0500

    Añade la interfaz del repositorio ProformaResponse
    Esta confirmación introduce un nuevo archivo, `proforma-response.repository.ts`, que contiene la interfaz `IProformaResponseRepository`. Esta interfaz extiende la interfaz `IRepository` y es responsable de definir los métodos relacionados con la entidad `ProformaResponse`. La incorporación de esta interfaz del repositorio permitirá la implementación de la funcionalidad necesaria para gestionar los objetos `ProformaResponse`.

---
+++ src/modules/proforma-response/application/repositories/proforma-response.repository.ts
@@ -0,0 +1,4 @@
+import { ProformaResponse } from "@Core/entities/proforma-response";
+import { IRepository } from "@Shared/repositories/repository";
+
+export interface IProformaResponseRepository extends IRepository<ProformaResponse> {}
