670cd860a376db99b47e3c1b6dcad40f2f122849
Henry Mosquera <henrym.718@hotmail.com>
2025-03-05 21:19:40 -0500

    Añade el mapeador ProformaResponse para persistencia
    Esta confirmación introduce un nuevo archivo, `proforma-reponse.mapper.ts`, que contiene la clase `ProformaReponseMapper`. Esta clase proporciona métodos estáticos para convertir objetos `ProformaResponse` hacia y desde el modelo de datos de Prisma. El método `toPersistence` asigna un objeto `ProformaResponse` al tipo `ProformaResponseCreateInput` de Prisma, mientras que el método `toDomain` realiza la asignación inversa. Este mapeador se utilizará para persistir y recuperar objetos `ProformaResponse` en la aplicación.

---
+++ src/modules/proforma-response/infraestructure/persistence/proforma-reponse.mapper.ts
@@ -0,0 +1,29 @@
+import { ProformaResponse } from "@Core/entities/proforma-response";
+import { IdVO, PriceVO, StatusVO, TextVO } from "@Core/value-objects";
+import { Prisma, ProformaResponse as PrismaProformaResponse } from "@prisma/client";
+
+export class ProformaReponseMapper {
+    public static toPersistence(entity: ProformaResponse): Prisma.ProformaResponseCreateInput {
+        return {
+            id: entity.id.getValue(),
+            budget: entity.budget.getValue(),
+            message: entity.message.getValue(),
+            status: entity.status.getValue(),
+            vendorProfile: { connect: { id: entity.profileVendorId.getValue() } },
+            proformaRequest: { connect: { id: entity.proformaRequestId.getValue() } },
+        };
+    }
+
+    public static toDomain(entity: PrismaProformaResponse): ProformaResponse {
+        return ProformaResponse.reconstitute({
+            id: IdVO.create(entity.id),
+            budget: PriceVO.create(entity.budget),
+            message: TextVO.create("message", entity.message),
+            status: StatusVO.fromPlainText(entity.status),
+            createdAt: entity.createdAt,
+            updatedAt: entity.updatedAt,
+            profileVendorId: IdVO.create(entity.profileVendorId),
+            proformaRequestId: IdVO.create(entity.proformaRequestId),
+        });
+    }
+}
