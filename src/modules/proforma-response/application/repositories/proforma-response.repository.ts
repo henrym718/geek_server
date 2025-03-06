4b5bf39855e35a951ee3d526004f486c4acfa35f
Henry Mosquera <henrym.718@hotmail.com>
2025-03-06 14:53:09 -0500

    Agrega un método para buscar ProformaResponse por ProformaRequestId y ProfileVendorId
    Esta confirmación agrega un nuevo método a la interfaz IProformaResponseRepository que permite buscar un ProformaResponse por su ProformaRequestId y ProfileVendorId. Este cambio mejora la funcionalidad del repositorio al proporcionar una manera conveniente de recuperar ProformaResponse según criterios específicos.

--- src/modules/proforma-response/application/repositories/proforma-response.repository.ts
+++ src/modules/proforma-response/application/repositories/proforma-response.repository.ts
@@ -1,4 +1,6 @@
 import { ProformaResponse } from "@Core/entities/proforma-response";
 import { IRepository } from "@Shared/repositories/repository";
 
-export interface IProformaResponseRepository extends IRepository<ProformaResponse> {}
+export interface IProformaResponseRepository extends IRepository<ProformaResponse> {
+    findByProformaRequestIdAndProfileVendorId(proformaRequestId: string, profileVendorId: string): Promise<boolean>;
+}
