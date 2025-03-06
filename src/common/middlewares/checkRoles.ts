c1b8e05b494f1ed16aec8f2035bf64e177a53494
Henry Mosquera <henrym.718@hotmail.com>
2025-03-05 21:12:24 -0500

    Mejorar el mensaje de error en el middleware checkRoles
    Mejorar el mensaje de error para incluir el rol del usuario no autorizado

--- src/common/middlewares/checkRoles.ts
+++ src/common/middlewares/checkRoles.ts
@@ -5,7 +5,7 @@ import { NextFunction, Request, Response } from "express";
 export const checkRoles = (...roles: RoleEnum[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
         if (!req.user) throw HttpException.unauthorized("Not authenticated");
-        if (!roles.includes(req.user.role as RoleEnum)) throw HttpException.forbidden("Not authorized");
+        if (!roles.includes(req.user.role as RoleEnum)) throw HttpException.forbidden(`Role ${req.user.role} Not authorized`);
         next();
     };
 };
