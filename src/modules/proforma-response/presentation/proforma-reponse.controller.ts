e12590f8022d93448ab398f14b89271d83ac9c3b
Henry Mosquera <henrym.718@hotmail.com>
2025-03-05 21:20:33 -0500

    Agrega un controlador de respuesta proforma para crear respuestas proforma
    Implementa un nuevo controlador para crear respuestas proforma en el módulo proforma-response. El controlador maneja la creación de respuestas proforma al procesar el cuerpo de la solicitud e invocar el caso de uso apropiado. Luego envía una respuesta de éxito con los detalles generados. Esta incorporación habilita la funcionalidad para crear respuestas proforma en la aplicación.

---
+++ src/modules/proforma-response/presentation/proforma-reponse.controller.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import { ICreateProformaResponseUseCase } from "../application/use-cases/create/create-proforma-response.use-case";
+import { HttpResponse } from "@Common/response/http.response";
+import { ReqCreateProformaResponseDto } from "../application/use-cases/create/create-proforma-response.dto";
+import { inject, injectable } from "inversify";
+import { PROFORMA_RES_SYMBOLS } from "../infraestructure/container/proforma-reponse.symbols";
+
+@injectable()
+export class ProformaReponseController {
+    constructor(
+        @inject(PROFORMA_RES_SYMBOLS.CreateProformaResponseUseCase) private readonly createProformaResponseUseCase: ICreateProformaResponseUseCase
+    ) {
+        this.create = this.create.bind(this);
+    }
+
+    async create(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data: ReqCreateProformaResponseDto = req.body;
+            const { details } = await this.createProformaResponseUseCase.execute(data);
+            HttpResponse.success(res, null, details);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
