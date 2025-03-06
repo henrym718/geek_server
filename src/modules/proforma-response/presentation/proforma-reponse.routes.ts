fa9ddf3369c6e4eb8735c931b0da57336a4aa29c
Henry Mosquera <henrym.718@hotmail.com>
2025-03-05 21:32:38 -0500

    Agrega rutas para manejar respuestas proforma con autenticación y verificación de roles
    Agrega rutas para manejar respuestas proforma con autenticación y verificación de roles
    
    Esta confirmación agrega nuevas rutas para manejar respuestas proforma con autenticación y verificación de roles. Permite que los usuarios autenticados con el rol de Proveedor creen respuestas proforma. Este cambio está motivado por la necesidad de proteger la funcionalidad de respuesta proforma y restringir el acceso a los usuarios autorizados.

---
+++ src/modules/proforma-response/presentation/proforma-reponse.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { proformaResponseContainer } from "../infraestructure/container/proforma-reponse.container";
+import { ProformaReponseController } from "./proforma-reponse.controller";
+import { authenticate } from "@Common/middlewares/authenticate";
+import { checkRoles } from "@Common/middlewares/checkRoles";
+import { RoleEnum } from "@Core/value-objects";
+
+export const proformaResponseRoutes = Router();
+const proformaResponseController = proformaResponseContainer.get(ProformaReponseController);
+
+proformaResponseRoutes.post("/", authenticate, checkRoles(RoleEnum.VENDOR), proformaResponseController.create);
