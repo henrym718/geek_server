ab87bf08f42235bd680da0c11c6257b949dd4340
Henry Mosquera <henrym.718@hotmail.com>
2025-03-09 12:40:53 -0500

    Refactoriza ProformaRequestsPrismaRepository para incluir relaciones y asignaciones adicionales
    Agrega inclusión de habilidades y categoría en el método findById
    Agrega el método findAllByClientId para recuperar ProformaRequests con información de habilidades y categoría

--- src/modules/proforma-requests/infraestructure/persistence/proforma-requests-prisma.repository.ts
+++ src/modules/proforma-requests/infraestructure/persistence/proforma-requests-prisma.repository.ts
@@ -1,7 +1,9 @@
 import { PrismaBootstrap } from "@Bootstraps/prisma.bootsrap";
 import { ProformaRequest } from "@Core/entities/proforma-requests";
-import { IProformaRequestsRepository } from "@ProformaRequests/application/repositories/proforma-requests.repository";
+import { IProformaRequestsRepository, ProformaRequestWithRelations } from "@ProformaRequests/application/repositories/proforma-requests.repository";
 import { ProformaRequestsMapper } from "./proforma-requests.mapper";
+import { SkillMapper } from "@Skill/infraestructure/persistence/skill.mapper";
+import { CategoryMapper } from "@Category/infraestructure/persistence/category.mapper";
 
 export class ProformaRequestsPrismaRepository implements IProformaRequestsRepository {
     private get db() {
@@ -19,11 +21,26 @@ export class ProformaRequestsPrismaRepository implements IProformaRequestsReposi
     }
 
     async findById(id: string): Promise<ProformaRequest | null> {
-        const proformaRequest = await this.db.findUnique({ where: { id } });
+        const proformaRequest = await this.db.findUnique({
+            where: { id },
+            include: { skills: { select: { id: true } } },
+        });
         return proformaRequest ? ProformaRequestsMapper.toDomain(proformaRequest) : null;
     }
 
     findAll(): Promise<ProformaRequest[]> {
         throw new Error("Method not implemented.");
+    }
+
+    async findAllByClientId(clientId: string): Promise<ProformaRequestWithRelations[]> {
+        const proformaRequests = await this.db.findMany({
+            where: { clientId },
+            include: { skills: true, category: true },
+        });
+        return proformaRequests.map((item) => ({
+            proformaRequest: ProformaRequestsMapper.toDomain(item),
+            skills: item.skills.map((s) => SkillMapper.toDomain(s)),
+            category: CategoryMapper.toDomain(item.category),
+        }));
     }
 }
