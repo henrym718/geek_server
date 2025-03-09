50e124dd2a2bf755c9d84cb4a7259ab50d4b4de5
Henry Mosquera <henrym.718@hotmail.com>
2025-03-09 12:42:59 -0500

    Agrega el método findAllByClientId a ProformaRequestsRepository
    Esta confirmación agrega un nuevo método, `findAllByClientId`, a `ProformaRequestsRepository`. Este método permite encontrar todas las solicitudes proforma asociadas con un ID de cliente específico. Este cambio mejora la funcionalidad del repositorio al proporcionar una manera de recuperar solicitudes proforma basadas en un ID de cliente.

--- src/modules/proforma-requests/application/repositories/proforma-requests.repository.ts
+++ src/modules/proforma-requests/application/repositories/proforma-requests.repository.ts
@@ -1,4 +1,13 @@
 import { ProformaRequest } from "@Core/entities/proforma-requests";
+import { Skill } from "@Core/entities/skill";
+import { Category } from "@Core/entities/category";
 import { IRepository } from "@Shared/repositories/repository";
 
-export interface IProformaRequestsRepository extends IRepository<ProformaRequest> {}
+export interface ProformaRequestWithRelations {
+    proformaRequest: ProformaRequest;
+    skills: Skill[];
+    category: Category;
+}
+export interface IProformaRequestsRepository extends IRepository<ProformaRequest> {
+    findAllByClientId(clientId: string): Promise<ProformaRequestWithRelations[]>;
+}
