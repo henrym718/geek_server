124c483d5c359237fda6596a5b4ece4ff6ca4049
Henry Mosquera <henrym.718@hotmail.com>
2025-03-09 12:44:12 -0500

    Refactorizar el contenedor y agregar un nuevo caso de uso para el módulo de respuesta proforma
    Esta confirmación refactoriza el contenedor en el módulo de respuesta proforma actualizando las rutas de importación y las dependencias de enlace. También agrega un nuevo caso de uso, 'GetProformaResponsesByRequestId', para recuperar respuestas proforma por ID de solicitud proforma.

--- src/modules/proforma-response/infraestructure/container/proforma-reponse.container.ts
+++ src/modules/proforma-response/infraestructure/container/proforma-reponse.container.ts
@@ -2,8 +2,8 @@ import { Container } from "inversify";
 import { PROFORMA_RES_SYMBOLS } from "./proforma-reponse.symbols";
 import { IProformaResponseRepository } from "modules/proforma-response/application/repositories/proforma-response.repository";
 import { ProformaResponsePrismaRepository } from "../persistence/proforma-reponse-prisma.repository";
-import { ICreateProformaResponseUseCase } from "modules/proforma-response/application/use-cases/create/create-proforma-response.use-case";
-import { CreateProformaResponseUseCase } from "modules/proforma-response/application/use-cases/create/create-proforma-response.impl";
+import { ICreateProformaResponseUseCase } from "modules/proforma-response/application/use-cases/create-proforma-response/create-proforma-response.use-case";
+import { CreateProformaResponseUseCase } from "modules/proforma-response/application/use-cases/create-proforma-response/create-proforma-response.impl";
 import { ProformaReponseController } from "modules/proforma-response/presentation/proforma-reponse.controller";
 import { IProformaRequestsRepository } from "@ProformaRequests/application/repositories/proforma-requests.repository";
 import { PROFORMA_REQ_SYMBOLS } from "@ProformaRequests/infraestructure/container/proforma-requests.symbols";
@@ -14,12 +14,18 @@ import { UUIDServiceImpl } from "@Shared/services/uuid/uuid.service.impl";
 import { IVendorProfilesRepository } from "@VendorProfile/aplication/repositories/vendor-profile.repository";
 import { VendorProfilePrismaRepository } from "@VendorProfile/infraestructure/persistence/vendor-profile-prisma.repository";
 import { VENDOR_PROFILE_SYMBOLS } from "@VendorProfile/infraestructure/container/vendor-profile.symbols";
+import { IGetProformaResponsesByRequestIdUseCase } from "modules/proforma-response/application/use-cases/get-proforma-responses-by-proforma-request-Id/get-proforma-responses-by-proforma-request-id.use-case";
+import { GetProformaResponsesByRequestIdUseCase } from "modules/proforma-response/application/use-cases/get-proforma-responses-by-proforma-request-Id/get-proforma-responses-by-proforma-request-id.impl.";
 
 export const proformaResponseContainer = new Container();
 
 proformaResponseContainer.bind<IProformaResponseRepository>(PROFORMA_RES_SYMBOLS.ProformaResponseRepository).to(ProformaResponsePrismaRepository);
-proformaResponseContainer.bind<ICreateProformaResponseUseCase>(PROFORMA_RES_SYMBOLS.CreateProformaResponseUseCase).to(CreateProformaResponseUseCase);
 proformaResponseContainer.bind<IVendorProfilesRepository>(VENDOR_PROFILE_SYMBOLS.VendorProfileRepository).to(VendorProfilePrismaRepository);
 proformaResponseContainer.bind<IProformaRequestsRepository>(PROFORMA_REQ_SYMBOLS.ProformaRequestsRepository).to(ProformaRequestsPrismaRepository);
 proformaResponseContainer.bind<IUUIDService>(SHARED_SYMBOLS.UUIDService).to(UUIDServiceImpl);
+proformaResponseContainer.bind<ICreateProformaResponseUseCase>(PROFORMA_RES_SYMBOLS.CreateProformaResponseUseCase).to(CreateProformaResponseUseCase);
+proformaResponseContainer
+    .bind<IGetProformaResponsesByRequestIdUseCase>(PROFORMA_RES_SYMBOLS.GetproformaResponsesByRequestId)
+    .to(GetProformaResponsesByRequestIdUseCase);
+
 proformaResponseContainer.bind<ProformaReponseController>(ProformaReponseController).toSelf();
--- src/modules/proforma-response/infraestructure/container/proforma-reponse.symbols.ts
+++ src/modules/proforma-response/infraestructure/container/proforma-reponse.symbols.ts
@@ -1,4 +1,5 @@
 export const PROFORMA_RES_SYMBOLS = {
     ProformaResponseRepository: Symbol.for("ProformaResponseRepository"),
     CreateProformaResponseUseCase: Symbol.for("CreateProformaResponseUseCase"),
+    GetproformaResponsesByRequestId: Symbol.for("GetproformaResponsesByRequestId"),
 };
