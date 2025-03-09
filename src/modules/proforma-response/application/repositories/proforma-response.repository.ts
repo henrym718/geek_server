949bc152ab2bd8461435787059b78e38967557a9
Henry Mosquera <henrym.718@hotmail.com>
2025-03-09 12:43:36 -0500

    Agrega el método findAllByRequestId a ProformaResponseRepository
    Esta confirmación agrega un nuevo método, `findAllByRequestId`, a la interfaz `IProformaResponseRepository`. Este método permite encontrar todos los objetos `ProformaResponse` asociados con un ID de solicitud específico. Este cambio mejora la funcionalidad del repositorio al proporcionar una manera conveniente de recuperar múltiples respuestas basadas en un ID de solicitud.

--- src/modules/proforma-response/application/repositories/proforma-response.repository.ts
+++ src/modules/proforma-response/application/repositories/proforma-response.repository.ts
@@ -1,6 +1,16 @@
 import { ProformaResponse } from "@Core/entities/proforma-response";
+import { Vendor } from "@Core/entities/vendor";
+import { VendorProfile } from "@Core/entities/profile-vendor";
+
 import { IRepository } from "@Shared/repositories/repository";
+
+export interface ProformaResponseWithVendor {
+    proformaResponse: ProformaResponse;
+    vendor: Vendor;
+    vendorProfile: VendorProfile;
+}
 
 export interface IProformaResponseRepository extends IRepository<ProformaResponse> {
     findByProformaRequestIdAndProfileVendorId(proformaRequestId: string, profileVendorId: string): Promise<boolean>;
+    findAllByRequestId(requestId: string): Promise<ProformaResponseWithVendor[]>;
 }
