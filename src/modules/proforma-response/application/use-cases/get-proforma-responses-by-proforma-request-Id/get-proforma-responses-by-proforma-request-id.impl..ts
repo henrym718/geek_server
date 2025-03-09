646127d4e7e855ee331be97b8da75097017c17ba
Henry Mosquera <henrym.718@hotmail.com>
2025-03-09 12:45:27 -0500

    Agrega GetProformaResponsesByRequestIdUseCase para recuperar respuestas proforma por ID de solicitud
    Esta confirmación agrega la clase GetProformaResponsesByRequestIdUseCase, que implementa la interfaz IGetProformaResponsesByRequestIdUseCase. El caso de uso es responsable de recuperar respuestas proforma por el ID de solicitud dado. Utiliza ProformaResponseRepository para obtener las respuestas y asigna los datos al formato ResGetProformaResponsesByRequestIdDto mediante el método proformaResponseMapper. El propósito de este cambio es habilitar la recuperación de respuestas proforma en función de un ID de solicitud específico.

---
+++ src/modules/proforma-response/application/use-cases/get-proforma-responses-by-proforma-request-Id/get-proforma-responses-by-proforma-request-id.impl..ts
@@ -0,0 +1,40 @@
+import { IdVO } from "@Core/value-objects";
+import { IProformaResponseRepository, ProformaResponseWithVendor } from "../../repositories/proforma-response.repository";
+import { ReqGetProformaResponsesByRequestIdDto, ResGetProformaResponsesByRequestIdDto } from "./get-proforma-responses-by-proforma-request-id.dto";
+import { IGetProformaResponsesByRequestIdUseCase } from "./get-proforma-responses-by-proforma-request-id.use-case";
+import { inject, injectable } from "inversify";
+import { PROFORMA_RES_SYMBOLS } from "modules/proforma-response/infraestructure/container/proforma-reponse.symbols";
+
+@injectable()
+export class GetProformaResponsesByRequestIdUseCase implements IGetProformaResponsesByRequestIdUseCase {
+    constructor(
+        @inject(PROFORMA_RES_SYMBOLS.ProformaResponseRepository)
+        private readonly proformaResponseRepository: IProformaResponseRepository
+    ) {}
+
+    async execute(data: ReqGetProformaResponsesByRequestIdDto): Promise<ResGetProformaResponsesByRequestIdDto[]> {
+        const requestId = IdVO.create(data.requestId);
+        const reponses = await this.proformaResponseRepository.findAllByRequestId(requestId.getValue());
+
+        return this.proformaResponseMapper(reponses);
+    }
+
+    private proformaResponseMapper(data: ProformaResponseWithVendor[]): ResGetProformaResponsesByRequestIdDto[] {
+        return data.map(({ proformaResponse, vendor, vendorProfile }) => ({
+            id: proformaResponse.id.getValue(),
+            budget: proformaResponse.budget.getValue(),
+            message: proformaResponse.message.getValue(),
+            status: proformaResponse.status.getValue(),
+            createdAt: proformaResponse.updatedAt,
+            vendor: {
+                id: vendor.id.getValue(),
+                firstName: vendor.firstName.getValue(),
+                lastName: vendor.lastName.getValue(),
+                photo: vendor.photo.getValue(),
+            },
+            vendorProfile: {
+                id: vendorProfile.id.getValue(),
+            },
+        }));
+    }
+}
