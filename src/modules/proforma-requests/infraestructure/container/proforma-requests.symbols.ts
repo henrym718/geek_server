dfbb12b28ae76b7420d43dadf28924d45b4be111
Henry Mosquera <henrym.718@hotmail.com>
2025-03-13 13:14:00 -0500

    Se refactoriza el contenedor de solicitudes proforma con dependencias actualizadas y nuevos casos de uso.
    Se actualiza el contenedor de solicitudes proforma con nuevas dependencias y casos de uso para crear y recuperar solicitudes proforma por perfil de cliente y proveedor. También se incluyen cambios en los enlaces del repositorio y la incorporación de servicios compartidos.

--- src/modules/proforma-requests/infraestructure/container/proforma-requests.symbols.ts
+++ src/modules/proforma-requests/infraestructure/container/proforma-requests.symbols.ts
@@ -1,6 +1,7 @@
 export const PROFORMA_REQ_SYMBOLS = {
     ProformaRequestsRepository: Symbol.for("ProformaRequestsRepository"),
     CreateProformaRequestsUseCase: Symbol.for("CreateProformaRequestsUseCase"),
-    GetProformaRequestsByClientIdUseCase: Symbol.for("GetProformaRequestsByClientIdUseCase"),
+    GetProformaRequestsByClientUseCase: Symbol.for("GetProformaRequestsByClientUseCase"),
     CanceledProformaRequestUseCase: Symbol.for("CanceledProformaRequestUseCase"),
+    GetProformaRequestsByVendorProfile: Symbol.for("GetProformaRequestsByVendorProfile"),
 };
