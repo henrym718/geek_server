924f0181d3e59581611bec48638919107d2f815e
Henry Mosquera <henrym.718@hotmail.com>
2025-03-13 13:33:49 -0500

    Se elimina la declaración de importación no utilizada del repositorio de perfiles de proveedor.
    Se elimina la declaración de importación no utilizada de la entidad Habilidad del repositorio de perfiles de proveedor. Este cambio mejora la limpieza del código y reduce las dependencias innecesarias.

--- src/modules/vendor-profile/aplication/repositories/vendor-profile.repository.ts
+++ src/modules/vendor-profile/aplication/repositories/vendor-profile.repository.ts
@@ -1,5 +1,4 @@
 import { VendorProfile } from "@Core/entities/profile-vendor";
-import { Skill } from "@Core/entities/skill";
 import { IRepository } from "@Shared/repositories/repository";
 
 export interface IVendorProfilesRepository extends IRepository<VendorProfile> {
