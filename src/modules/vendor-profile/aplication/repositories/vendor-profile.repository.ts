d14ef76f8722d93a33da80afdf6488b09549ba44
Henry Mosquera <henrym.718@hotmail.com>
2025-03-15 16:48:30 -0500

    Añade funcionalidad de búsqueda a los perfiles de proveedores.
    Implementa el método searchVendorProfiles en la interfaz IVendorProfilesRepository y la clase VendorProfilePrismaRepository. Este método permite buscar perfiles de proveedores según diversos criterios, como puesto, habilidades, categoría y ubicación. Devuelve una lista de perfiles de proveedores junto con el proveedor y las habilidades asociadas. También incluye el recuento total de resultados de la búsqueda.

--- src/modules/vendor-profile/aplication/repositories/vendor-profile.repository.ts
+++ src/modules/vendor-profile/aplication/repositories/vendor-profile.repository.ts
@@ -1,6 +1,13 @@
 import { VendorProfile } from "@Core/entities/profile-vendor";
+import { Vendor } from "@Core/entities/vendor";
 import { IRepository } from "@Shared/repositories/repository";
+import { SearchRequest } from "../use-cases/search-vendor-profiles/search-vendor-profiles.dto";
+import { Skill } from "@Core/entities/skill";
 
 export interface IVendorProfilesRepository extends IRepository<VendorProfile> {
     findByIdWithSkillsId(id: string): Promise<VendorProfile | null>;
+    searchVendorProfiles(filter: Required<SearchRequest>): Promise<{
+        vendorProfiles: Array<{ vendorProfile: VendorProfile; vendor: Vendor; skills: Skill[] }>;
+        results: number;
+    }>;
 }
--- src/modules/vendor-profile/infraestructure/persistence/vendor-profile-prisma.repository.ts
+++ src/modules/vendor-profile/infraestructure/persistence/vendor-profile-prisma.repository.ts
@@ -2,6 +2,11 @@ import { VendorProfile } from "@Core/entities/profile-vendor";
 import { IVendorProfilesRepository } from "@VendorProfile/aplication/repositories/vendor-profile.repository";
 import { PrismaBootstrap } from "@Bootstraps/prisma.bootsrap";
 import { VendorProfileMapper } from "./vendor-profile.mapper";
+import { Vendor } from "@Core/entities/vendor";
+import { Skill } from "@Core/entities/skill";
+import { SearchRequest } from "@VendorProfile/aplication/use-cases/search-vendor-profiles/search-vendor-profiles.dto";
+import { SkillMapper } from "@Skill/infraestructure/persistence/skill.mapper";
+import { VendorMapper } from "@Vendor/infraestructure/persistence/vendor.mapper";
 
 export class VendorProfilePrismaRepository implements IVendorProfilesRepository {
     get db() {
@@ -24,6 +29,47 @@ export class VendorProfilePrismaRepository implements IVendorProfilesRepository 
     async findByIdWithSkillsId(id: string): Promise<VendorProfile | null> {
         const response = await this.db.vendorProfile.findUnique({ where: { id }, include: { skills: true } });
         return response ? VendorProfileMapper.toDomain(response, response.skills) : null;
+    }
+
+    async searchVendorProfiles(
+        filter: Required<SearchRequest>
+    ): Promise<{ vendorProfiles: Array<{ vendorProfile: VendorProfile; vendor: Vendor; skills: Skill[] }>; results: number }> {
+        const { order, city, skills, page, query, categoryName, limit } = filter;
+        const take = limit;
+        const skip = (page - 1) * take;
+
+        const whereCondition = {
+            OR: [
+                { tittle: { contains: query, mode: "insensitive" as const } },
+                { skills: { some: { name: { contains: query, mode: "insensitive" as const } } } },
+            ],
+            category: { name: categoryName },
+            vendor: { city: { contains: city, mode: "insensitive" as const } },
+            AND: skills?.split(",").map((skill) => ({ skills: { some: { name: skill } } })),
+            isActive: true,
+        };
+
+        const [profiles, results] = await this.db.$transaction([
+            this.db.vendorProfile.findMany({
+                where: whereCondition,
+                orderBy: { createdAt: order },
+                take,
+                skip,
+                include: { skills: true, vendor: true },
+            }),
+
+            this.db.vendorProfile.count({
+                where: whereCondition,
+            }),
+        ]);
+
+        const vendorProfiles = profiles.map(({ vendor, skills, ...vendorProfile }) => ({
+            vendorProfile: VendorProfileMapper.toDomain(vendorProfile),
+            vendor: VendorMapper.toDomain(vendor),
+            skills: skills.map((skill) => SkillMapper.toDomain(skill)),
+        }));
+
+        return { vendorProfiles, results };
     }
 
     findAll(): Promise<VendorProfile[]> {
