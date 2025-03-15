be5f8033e598606eea38abc5530ffeba054d792a
Henry Mosquera <henrym.718@hotmail.com>
2025-03-15 18:10:22 -0500

    Se refactoriza SkillController para usar las rutas de módulo actualizadas.
    Se actualizan las rutas de importación en SkillController para usar la nueva estructura de módulo.
    Se añade la función SearchSkillsSuggestions a SkillController.

--- src/modules/skill/presentation/skill.controller.ts
+++ src/modules/skill/presentation/skill.controller.ts
@@ -1,19 +1,39 @@
 import { NextFunction, Request, Response } from "express";
-import { ICreateSkillUseCase } from "../application/use-case/create/create-skill.use-case";
+import { ICreateSkillUseCase } from "@Skill/application/use-case/create-skill/create-skill.use-case";
 import { HttpResponse } from "@Common/response/http.response";
-import { ReqCreateSkillDto } from "../application/use-case/create/create-skill.dto";
+import { ReqCreateSkillDto } from "@Skill/application/use-case/create-skill/create-skill.dto";
 import { inject, injectable } from "inversify";
 import { SKILL_SYMBOLS } from "../infraestructure/container/skill.symbols";
+import { ISearchSkillsSuggestionsUseCase } from "@Skill/application/use-case/search-skills-suggestions/search-skills-suggestions.use-case";
+import { SearchSkillsRequest } from "@Skill/application/use-case/search-skills-suggestions/search-skills-suggestions.dto";
 
 @injectable()
 export class SkillController {
-    constructor(@inject(SKILL_SYMBOLS.CreateSkillUseCase) private readonly createSkillUseCase: ICreateSkillUseCase) {}
+    constructor(
+        @inject(SKILL_SYMBOLS.CreateSkill)
+        private readonly createSkillUseCase: ICreateSkillUseCase,
 
-    async create(req: Request, res: Response, next: NextFunction) {
+        @inject(SKILL_SYMBOLS.SearchSkillsSuggestions)
+        private readonly searchSkillsSuggestionsUseCase: ISearchSkillsSuggestionsUseCase
+    ) {
+        this.createSkill = this.createSkill.bind(this);
+        this.searchSkillsSuggestions = this.searchSkillsSuggestions.bind(this);
+    }
+
+    async createSkill(req: Request, res: Response, next: NextFunction) {
         try {
             const data: ReqCreateSkillDto = req.body;
             const { detail } = await this.createSkillUseCase.execute(data);
             HttpResponse.success(res, null, detail);
+        } catch (error) {
+            next(error);
+        }
+    }
+    async searchSkillsSuggestions(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data: SearchSkillsRequest = req.body;
+            const response = await this.searchSkillsSuggestionsUseCase.execute(data);
+            HttpResponse.success(res, response);
         } catch (error) {
             next(error);
         }
