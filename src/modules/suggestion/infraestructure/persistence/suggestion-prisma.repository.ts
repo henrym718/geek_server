f79270f3af802b96b4e429880b6d46bfb903f835
Henry Mosquera <henrym.718@hotmail.com>
2025-03-18 11:19:38 -0500

    Se refactoriza SuggestionPrismaRepository para incluir información sobre categorías y habilidades.
    Se actualiza SuggestionPrismaRepository añadiendo información sobre categorías y habilidades al método getSuggestionsBySearchText. Este cambio permite que el método devuelva una matriz de sugerencias junto con sus correspondientes detalles de categoría y habilidad. También se ha añadido el parámetro limit para limitar el número de sugerencias devueltas. Esta refactorización mejora la funcionalidad del repositorio y los datos que devuelve el método.

--- src/modules/suggestion/infraestructure/persistence/suggestion-prisma.repository.ts
+++ src/modules/suggestion/infraestructure/persistence/suggestion-prisma.repository.ts
@@ -2,19 +2,25 @@ import { PrismaBootstrap } from "@Bootstraps/prisma.bootsrap";
 import { Suggestion } from "@Core/entities/suggestion";
 import { ISuggestionRepository } from "modules/suggestion/application/repositories/suggestion.repository";
 import { SuggestionMapper } from "./suggestion.mapper";
+import { Category } from "@Core/entities/category";
+import { Skill } from "@Core/entities/skill";
+import { CategoryMapper } from "@Category/infraestructure/persistence/category.mapper";
+import { SkillMapper } from "@Skill/infraestructure/persistence/skill.mapper";
 
 export class SuggestionPrismaRepository implements ISuggestionRepository {
     private get db() {
         return PrismaBootstrap.prisma;
     }
-
-    async getSuggestionsBySearchText(searchText: string): Promise<Suggestion[]> {
+    async getSuggestionsBySearchText(searchText: string, limit: number): Promise<{ suggestions: Suggestion; category: Category; skills: Skill }[]> {
         const suggestions = await this.db.suggestion.findMany({
-            where: {
-                text: { contains: searchText, mode: "insensitive" },
-            },
-            include: { category: { select: { id: true } }, skill: { select: { id: true } } },
+            where: { text: { contains: searchText, mode: "insensitive" } },
+            include: { category: true, skill: true },
+            take: limit,
         });
-        return suggestions.map((suggestion) => SuggestionMapper.toDomain(suggestion));
+        return suggestions.map(({ skill, category, ...suggestions }) => ({
+            suggestions: SuggestionMapper.toDomain(suggestions),
+            category: CategoryMapper.toDomain(category),
+            skills: SkillMapper.toDomain(skill),
+        }));
     }
 }
