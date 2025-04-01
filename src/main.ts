36eb4aa4ab2ae4d1a7705e20e7bb2e4be467d308
Henry Mosquera <henrym.718@hotmail.com>
2025-04-01 11:53:35 -0500

    Refactoriza la inicialización de la aplicación en main.ts. Se eliminan las instancias de EnvBootstrap, PrismaBootstrap y ServerBootstrap, simplificando la creación del objeto AppBootstrap. Esto mejora la claridad del código y facilita futuras modificaciones.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { AppBootstrap } from "@Bootstraps/app.bootstrap";
-import { EnvBootstrap } from "@Bootstraps/env.bootstrap";
-import { PrismaBootstrap } from "@Bootstraps/prisma.bootsrap";
-import { ServerBootstrap } from "@Bootstraps/server.bootsrap";
-
-const app = new AppBootstrap(new EnvBootstrap(), new PrismaBootstrap(), new ServerBootstrap());
 
 (async () => {
+    const app = new AppBootstrap();
     await app.initialize();
 })();
