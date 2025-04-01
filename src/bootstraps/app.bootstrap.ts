336d24ae78cd8be392ffd03ebf602df36cc3112e
Henry Mosquera <henrym.718@hotmail.com>
2025-04-01 11:54:14 -0500

    Refactoriza la clase AppBootstrap para incluir ContainerBootstrap en la inicialización de la aplicación. Se eliminan las instancias de las clases en el constructor y se crean dentro de él, mejorando la claridad y la estructura del código. Además, se corrige un error tipográfico en el método de inicialización de PrismaBootstrap.

--- src/bootstraps/app.bootstrap.ts
+++ src/bootstraps/app.bootstrap.ts
@@ -3,18 +3,26 @@ import { EnvBootstrap } from "./env.bootstrap";
 import { PrismaBootstrap } from "./prisma.bootsrap";
 import { ServerBootstrap } from "./server.bootsrap";
 import "../types/express";
+import { ContainerBootstrap } from "./container.bootstrap";
 
 export class AppBootstrap {
-    constructor(
-        private readonly envBootstrap: EnvBootstrap,
-        private readonly prismaBootstrap: PrismaBootstrap,
-        private readonly serverBootstrap: ServerBootstrap
-    ) {}
+    private readonly envBootstrap: EnvBootstrap;
+    private readonly containerBootstrap: ContainerBootstrap;
+    private readonly prismaBootstrap: PrismaBootstrap;
+    private readonly serverBootstrap: ServerBootstrap;
+
+    constructor() {
+        this.envBootstrap = new EnvBootstrap();
+        this.containerBootstrap = new ContainerBootstrap();
+        this.prismaBootstrap = new PrismaBootstrap();
+        this.serverBootstrap = new ServerBootstrap();
+    }
 
     async initialize() {
         try {
             await this.envBootstrap.initialize();
-            await this.prismaBootstrap.inizialized();
+            await this.containerBootstrap.initialize();
+            await this.prismaBootstrap.inizialize();
             await this.serverBootstrap.initialize();
         } catch (error) {
             logger.error(error);
