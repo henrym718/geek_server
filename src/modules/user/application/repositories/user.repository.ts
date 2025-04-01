8a4c6a5a6599f7dedcb0c31c64187303a66a1918
Henry Mosquera <henrym.718@hotmail.com>
2025-04-01 12:33:05 -0500

    Implementa la interfaz IUserRepository en el nuevo archivo user.repository.ts, definiendo métodos para crear, actualizar y buscar usuarios por correo electrónico, mejorando la organización del código y la gestión de datos de usuario.

--- src/modules/user/application/repositories/user.repository.ts
+++ src/modules/user/application/repositories/user.repository.ts
@@ -1,10 +1,10 @@
 import { Client } from "@Core/entities/client";
 import { User } from "@Core/entities/user";
 import { Vendor } from "@Core/entities/vendor";
-import { IRepository } from "@Shared/repositories/repository";
+import { IRepository } from "@Shared/interfaces/repository";
 
 export interface IUserRepository extends IRepository<User> {
-    create(user: User): Promise<void>;
+    create(user: User, ctx?: any): Promise<void>;
     update(user: User): Promise<void>;
     findbyEmail(email: string): Promise<User | null>;
     findUserByEmailWithProfile(email: string): Promise<{ user: User; vendor?: Vendor | null; client?: Client | null } | null>;
