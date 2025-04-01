acd1adbfbe49eebe71e246cf3afaf0bdfb952325
Henry Mosquera <henrym.718@hotmail.com>
2025-04-01 11:55:11 -0500

    Actualiza el middleware de autenticación para utilizar ContainerBootstrap en la obtención del servicio de token. Esto mejora la gestión de dependencias y la modularidad del código al integrar la nueva clase de inicialización de contenedores.

--- src/common/middlewares/authenticate.ts
+++ src/common/middlewares/authenticate.ts
@@ -1,8 +1,8 @@
 import { HttpException } from "@Common/exceptions/http.exception";
-import { sharedContainer } from "@Shared/container/shared.container";
 import { SHARED_SYMBOLS } from "@Shared/container/shared.symbols";
 import { ITokenService } from "@Shared/services/token/token.service";
 import { NextFunction, Request, Response } from "express";
+import { ContainerBootstrap, IDENTIFIERS } from "@Bootstraps/container.bootstrap";
 
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
     const header = req.header("Authorization");
@@ -10,6 +10,7 @@ export const authenticate = (req: Request, res: Response, next: NextFunction) =>
     if (!header.startsWith("Bearer ")) throw HttpException.forbidden("Invalid token format");
 
     const token = header.split(" ")[1];
+    const sharedContainer = ContainerBootstrap.getModuleContainer(IDENTIFIERS.Auth);
     const tokenService = sharedContainer.get<ITokenService>(SHARED_SYMBOLS.TokenService);
     const decoded = tokenService.verifyToken(token);
     req.user = decoded;
