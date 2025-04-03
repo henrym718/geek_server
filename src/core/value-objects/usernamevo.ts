64969e9ade4932a316fd116fd0cf158b495df85f
Henry Mosquera <henrym.718@hotmail.com>
2025-04-02 19:12:58 -0500

    Agregue la clase UsernameVO para la validación y encapsulación de nombres de usuario, garantizando que los nombres de usuario cumplan con los requisitos de longitud mínima.

---
+++ src/core/value-objects/usernamevo.ts
@@ -0,0 +1,24 @@
+export class UsernameVO {
+    private readonly value: string;
+
+    private constructor(value: string) {
+        this.value = value;
+    }
+
+    public static create(value: string): UsernameVO {
+        const username = value.trim();
+        console.log("username", username);
+        this.validate(username);
+        return new UsernameVO(value);
+    }
+
+    private static validate(value: string): void {
+        if (value.length < 3) {
+            throw new Error("Username must be at least 3 characters long");
+        }
+    }
+
+    public getValue(): string {
+        return this.value;
+    }
+}
