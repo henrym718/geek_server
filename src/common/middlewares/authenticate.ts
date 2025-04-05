b8f5944345054e5303bdb35f3ce3725f8259a580
Henry Mosquera <henrym.718@hotmail.com>
2025-04-04 19:56:05 -0500

    Refactoriza el middleware de autenticación para permitir la obtención del token de acceso desde las cookies, mejorando la flexibilidad en la gestión de tokens. Se actualiza la lógica de validación para manejar tanto el token en el encabezado como el de las cookies.

--- src/common/middlewares/authenticate.ts
+++ src/common/middlewares/authenticate.ts
@@ -5,15 +5,24 @@ import { NextFunction, Request, Response } from "express";
 import { ContainerBootstrap, IDENTIFIERS } from "@Bootstraps/container.bootstrap";
 
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
-    const header = req.header("Authorization");
-    if (!header) throw HttpException.forbidden("Token not provided");
-    if (!header.startsWith("Bearer ")) throw HttpException.forbidden("Invalid token format");
+    let accessToken: string | undefined;
 
-    const token = header.split(" ")[1];
+    if (req.cookies["accessToken"]) {
+        accessToken = req.cookies["accessToken"];
+    }
+
+    if (!accessToken && req.header("Authorization")) {
+        const header = req.header("Authorization");
+        if (!header?.startsWith("Bearer ")) throw HttpException.forbidden("Invalid token format");
+        accessToken = header?.split(" ")[1];
+    }
+    if (!accessToken) {
+        throw HttpException.forbidden("Token not provided");
+    }
+
     const sharedContainer = ContainerBootstrap.getModuleContainer(IDENTIFIERS.Auth);
     const tokenService = sharedContainer.get<ITokenService>(SHARED_SYMBOLS.TokenService);
-    const decoded = tokenService.verifyToken(token);
+    const decoded = tokenService.verifyToken(accessToken);
     req.user = decoded;
-
     next();
 };
