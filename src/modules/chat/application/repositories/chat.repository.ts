ce4c560cc30a1241c48ea60b0276feb1471e8627
Henry Mosquera <henrym.718@hotmail.com>
2025-04-24 13:14:31 -0500

    Actualiza la interfaz IChatRepository para que el método findAllByUser devuelva un array de objetos que incluyen el chat y los usuarios cliente y vendedor, mejorando la gestión de datos en la funcionalidad de chat.

--- src/modules/chat/application/repositories/chat.repository.ts
+++ src/modules/chat/application/repositories/chat.repository.ts
@@ -1,8 +1,9 @@
 import { Chat } from "@Core/entities/chat";
+import { User } from "@Core/entities/user";
 
 export interface IChatRepository {
     create(chat: Chat): Promise<Chat>;
     findById(id: string): Promise<Chat | null>;
-    findAllByUser(clientId: string): Promise<Chat[]>;
+    findAllByUser(clientId: string): Promise<{ chat: Chat; client: User; vendor: User }[]>;
     findByUsers(clientId: string, vendorId: string): Promise<Chat | null>;
 }
