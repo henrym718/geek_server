682a4a21cb10494eb18bd916e70cca8847e9b117
Henry Mosquera <henrym.718@hotmail.com>
2025-04-24 13:15:14 -0500

    Agregue el caso de uso GetChatsByUserId al contenedor de inyección de dependencias, mejorando la funcionalidad para recuperar chats específicos del usuario.

--- src/modules/chat/infraestructure/container/chat.container.ts
+++ src/modules/chat/infraestructure/container/chat.container.ts
@@ -3,12 +3,16 @@ import { CreateChatUseCase } from "modules/chat/application/use-cases/create-cha
 import { CHAT_SYMBOLS } from "./chat.symbols";
 import { registerControllers, registerUseCases } from "@Common/utils/container-utils";
 import { ChatController } from "modules/chat/presentation/chat.controller";
+import { GetChatsByUserIdUseCase } from "modules/chat/application/use-cases/get-chats-by-userid/get-chats-by-userid.impl";
 
 export function createChatContainer(parentContainer: Container): Container {
     const container = new Container();
     container.parent = parentContainer;
 
-    registerUseCases(container, [{ symbol: CHAT_SYMBOLS.CreateChat, implementation: CreateChatUseCase }]);
+    registerUseCases(container, [
+        { symbol: CHAT_SYMBOLS.CreateChat, implementation: CreateChatUseCase },
+        { symbol: CHAT_SYMBOLS.GetChatsByUserId, implementation: GetChatsByUserIdUseCase },
+    ]);
 
     registerControllers(container, [ChatController]);
 
--- src/modules/chat/infraestructure/container/chat.symbols.ts
+++ src/modules/chat/infraestructure/container/chat.symbols.ts
@@ -1,3 +1,4 @@
 export const CHAT_SYMBOLS = {
     CreateChat: Symbol("CreateChat"),
+    GetChatsByUserId: Symbol("GetChatsByUserId"),
 };
