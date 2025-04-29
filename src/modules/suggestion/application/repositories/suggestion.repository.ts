337178169515732677a21eea8237d8800ffa7171
Henry Mosquera <henrym.718@hotmail.com>
2025-04-29 08:45:52 -0500

    Refactorice ISuggestionRepository y SuggestionPrismaRepository para eliminar la categoría del método getSuggestionsBySearchText, simplificando así la estructura de la respuesta. Actualice SuggestionController para que se ajuste a la nueva firma del método, mejorando así la claridad y la facilidad de mantenimiento del código.

--- src/modules/suggestion/application/repositories/suggestion.repository.ts
+++ src/modules/suggestion/application/repositories/suggestion.repository.ts
@@ -1,7 +1,6 @@
-import { Category } from "@Core/entities/category";
 import { Skill } from "@Core/entities/skill";
 import { Suggestion } from "@Core/entities/suggestion";
 
 export interface ISuggestionRepository {
-    getSuggestionsBySearchText(searchText: string, limit: number): Promise<{ suggestions: Suggestion; category: Category; skills: Skill }[]>;
+    getSuggestionsBySearchText(): Promise<{ suggestions: Suggestion; skills: Skill }[]>;
 }
--- src/modules/suggestion/infraestructure/persistence/suggestion-prisma.repository.ts
+++ src/modules/suggestion/infraestructure/persistence/suggestion-prisma.repository.ts
@@ -2,24 +2,19 @@ import { PrismaBootstrap } from "@Bootstraps/prisma.bootsrap";
 import { Suggestion } from "@Core/entities/suggestion";
 import { ISuggestionRepository } from "modules/suggestion/application/repositories/suggestion.repository";
 import { SuggestionMapper } from "./suggestion.mapper";
-import { Category } from "@Core/entities/category";
 import { Skill } from "@Core/entities/skill";
-import { CategoryMapper } from "@Category/infraestructure/persistence/category.mapper";
 import { SkillMapper } from "@Skill/infraestructure/persistence/skill.mapper";
 
 export class SuggestionPrismaRepository implements ISuggestionRepository {
     private get db() {
         return PrismaBootstrap.prisma;
     }
-    async getSuggestionsBySearchText(searchText: string, limit: number): Promise<{ suggestions: Suggestion; category: Category; skills: Skill }[]> {
+    async getSuggestionsBySearchText(): Promise<{ suggestions: Suggestion; skills: Skill }[]> {
         const suggestions = await this.db.suggestion.findMany({
-            where: { text: { contains: searchText, mode: "insensitive" } },
-            include: { category: true, skill: true },
-            take: limit,
+            include: { skill: true },
         });
-        return suggestions.map(({ skill, category, ...suggestions }) => ({
+        return suggestions.map(({ skill, ...suggestions }) => ({
             suggestions: SuggestionMapper.toDomain(suggestions),
-            category: CategoryMapper.toDomain(category),
             skills: SkillMapper.toDomain(skill),
         }));
     }
