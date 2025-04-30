f225bddb6e3d1576e1fe781a719a91b9ae55805c
Henry Mosquera <henrym.718@hotmail.com>
2025-04-30 16:20:46 -0500

    Refactorizar el contenedor de perfil de proveedor para utilizar funciones de registro de casos de uso y controladores, y agregar el caso de uso GetVendorProfileById, mejorando la organización y la claridad del código.

--- src/modules/vendor-profile/infraestructure/container/vendor-profile.container.ts
+++ src/modules/vendor-profile/infraestructure/container/vendor-profile.container.ts
@@ -1,22 +1,22 @@
 import { Container } from "inversify";
 import { VENDOR_PROFILE_SYMBOLS } from "./vendor-profile.symbols";
-import { ICreateVendorProfileUseCase } from "@VendorProfile/aplication/use-cases/create-vendor-profile/create-vendor-profile.use-case";
 import { CreateVendorProfileUseCase } from "@VendorProfile/aplication/use-cases/create-vendor-profile/create-vendor-profile.impl";
 import { VendorProfileController } from "@VendorProfile/presentation/vendor-profile.controller";
-
-import { ISearchVendorProfilesUseCase } from "@VendorProfile/aplication/use-cases/search-vendor-profiles/search-vendor-profiles.use-case";
 import { SearchVendorProfilesUseCase } from "@VendorProfile/aplication/use-cases/search-vendor-profiles/search-vendor-profiles.impl";
-import { IGetVendorProfilesUseCase } from "@VendorProfile/aplication/use-cases/get-vendor-profiles-by-username/get-vendor-profiles.use-case";
-import { GetVendorProfilesUseCase } from "@VendorProfile/aplication/use-cases/get-vendor-profiles-by-username/get-vendor-profiles.impl";
+import { GetVendorProfileByIdUseCase } from "@VendorProfile/aplication/use-cases/get-vendor-profile-by-id/get-vendor-profiles-by-id.impl";
+import { registerUseCases, registerControllers } from "@Common/utils/container-utils";
 
 export function configureVendorProfileContainer(parentContainer: Container): Container {
     const container = new Container();
     container.parent = parentContainer;
 
-    container.bind<ICreateVendorProfileUseCase>(VENDOR_PROFILE_SYMBOLS.CreateVendorProfile).to(CreateVendorProfileUseCase);
-    container.bind<ISearchVendorProfilesUseCase>(VENDOR_PROFILE_SYMBOLS.SearchVendorProfiles).to(SearchVendorProfilesUseCase);
-    container.bind<IGetVendorProfilesUseCase>(VENDOR_PROFILE_SYMBOLS.GetVendorProfiles).to(GetVendorProfilesUseCase);
-    container.bind<VendorProfileController>(VendorProfileController).toSelf();
+    registerUseCases(container, [
+        { symbol: VENDOR_PROFILE_SYMBOLS.CreateVendorProfile, implementation: CreateVendorProfileUseCase },
+        { symbol: VENDOR_PROFILE_SYMBOLS.SearchVendorProfiles, implementation: SearchVendorProfilesUseCase },
+        { symbol: VENDOR_PROFILE_SYMBOLS.GetVendorProfileById, implementation: GetVendorProfileByIdUseCase },
+    ]);
+
+    registerControllers(container, [VendorProfileController]);
 
     return container;
 }
--- src/modules/vendor-profile/infraestructure/container/vendor-profile.symbols.ts
+++ src/modules/vendor-profile/infraestructure/container/vendor-profile.symbols.ts
@@ -2,5 +2,5 @@ export const VENDOR_PROFILE_SYMBOLS = {
     VendorProfileRepository: Symbol.for("VendorProfileRepository"),
     CreateVendorProfile: Symbol.for("CreateVendorProfile"),
     SearchVendorProfiles: Symbol.for("SearchVendorProfiles"),
-    GetVendorProfiles: Symbol.for("GetVendorProfiles"),
+    GetVendorProfileById: Symbol.for("GetVendorProfileById"),
 };
