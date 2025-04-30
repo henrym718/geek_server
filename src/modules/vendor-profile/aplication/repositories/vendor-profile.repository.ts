bbb0403a039234b13cefc88a9b73031e6825df48
Henry Mosquera <henrym.718@hotmail.com>
2025-04-30 16:20:08 -0500

    Refactorizar la interfaz IVendorProfilesRepository para eliminar la dependencia de IRepository y actualizar los métodos de búsqueda, mejorando la claridad y la estructura de la respuesta al incluir detalles del usuario y habilidades.

--- src/modules/vendor-profile/aplication/repositories/vendor-profile.repository.ts
+++ src/modules/vendor-profile/aplication/repositories/vendor-profile.repository.ts
@@ -1,11 +1,16 @@
 import { VendorProfile } from "@Core/entities/profile-vendor";
 import { Vendor } from "@Core/entities/vendor";
-import { IRepository } from "@Shared/interfaces/repository";
 import { SearchRequest } from "../use-cases/search-vendor-profiles/search-vendor-profiles.dto";
 import { Skill } from "@Core/entities/skill";
 import { Category } from "@Core/entities/category";
+import { User } from "@Core/entities/user";
 
-export interface IVendorProfilesRepository extends IRepository<VendorProfile> {
+export interface IVendorProfilesRepository {
+    create(entity: VendorProfile): Promise<void>;
+    findById(id: string): Promise<VendorProfile | null>;
+    findByIdWithDetails(
+        id: string
+    ): Promise<{ user: User; vendor: Vendor; vendorProfile: VendorProfile; skills: Skill[]; category: Category } | null>;
     findByIdWithSkillsId(id: string): Promise<VendorProfile | null>;
     searchVendorProfiles(filter: Required<SearchRequest>): Promise<{
         data: Array<{ vendorProfile: VendorProfile; vendor: Vendor }>;
