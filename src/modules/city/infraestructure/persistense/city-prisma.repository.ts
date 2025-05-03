60b45d77de6dc7267f5c708e898e5f737edd5a36
Henry Mosquera <henrym.718@hotmail.com>
2025-05-03 12:51:25 -0500

    Implementar el repositorio CityPrismaRepository para la recuperación de datos de ciudades, incluyendo el método findAll que devuelve una lista de entidades City utilizando Prisma y un mapeador para la conversión de datos.

---
+++ src/modules/city/infraestructure/persistense/city-prisma.repository.ts
@@ -0,0 +1,17 @@
+import { City } from "@Core/entities/city";
+import { ICityRepository } from "modules/city/application/repositories/city.repository";
+import { injectable } from "inversify";
+import { PrismaBootstrap } from "@Bootstraps/prisma.bootsrap";
+import { CityMapper } from "./city.mapper";
+
+@injectable()
+export class CityPrismaRepository implements ICityRepository {
+    private get db() {
+        return PrismaBootstrap.prisma;
+    }
+
+    async findAll(): Promise<City[]> {
+        const response = await this.db.city.findMany();
+        return response.map((city) => CityMapper.toDomain(city));
+    }
+}
