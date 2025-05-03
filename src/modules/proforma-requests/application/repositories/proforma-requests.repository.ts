7662edd1e947c4f8cca8900e68c5bb30e602298e
Henry Mosquera <henrym.718@hotmail.com>
2025-05-03 11:33:17 -0500

    Incorporar la entidad City en ProformaRequestWithRelations y actualizar ProformaRequestsPrismaRepository para incluir la ciudad en las consultas, mejorando la persistencia y la estructura de los datos de solicitudes proforma.

--- src/modules/proforma-requests/application/repositories/proforma-requests.repository.ts
+++ src/modules/proforma-requests/application/repositories/proforma-requests.repository.ts
@@ -3,11 +3,13 @@ import { Skill } from "@Core/entities/skill";
 import { Category } from "@Core/entities/category";
 import { IRepository } from "@Shared/interfaces/repository";
 import { StatusRequestVO } from "@Core/value-objects/status-request.vo";
+import { City } from "@Core/entities/city";
 
 export interface ProformaRequestWithRelations {
     proformaRequest: ProformaRequest;
     skills: Skill[];
     category: Category;
+    city: City;
 }
 export interface IProformaRequestsRepository extends IRepository<ProformaRequest> {
     findAllByClientId(clientId: string, status: StatusRequestVO): Promise<ProformaRequestWithRelations[]>;
--- src/modules/proforma-requests/infraestructure/persistence/proforma-requests-prisma.repository.ts
+++ src/modules/proforma-requests/infraestructure/persistence/proforma-requests-prisma.repository.ts
@@ -5,6 +5,7 @@ import { ProformaRequestsMapper } from "./proforma-requests.mapper";
 import { SkillMapper } from "@Skill/infraestructure/persistence/skill.mapper";
 import { CategoryMapper } from "@Category/infraestructure/persistence/category.mapper";
 import { StatusRequestVO } from "@Core/value-objects/status-request.vo";
+import { CityMapper } from "modules/city/infraestructure/persistense/city.mapper";
 
 export class ProformaRequestsPrismaRepository implements IProformaRequestsRepository {
     private get db() {
@@ -43,13 +44,14 @@ export class ProformaRequestsPrismaRepository implements IProformaRequestsReposi
                     },
                 ],
             },
-            include: { skills: true, category: true },
+            include: { skills: true, category: true, city: true },
         });
 
         return requests.map((item) => ({
             proformaRequest: ProformaRequestsMapper.toDomain(item),
             skills: item.skills.map((skill) => SkillMapper.toDomain(skill)),
             category: CategoryMapper.toDomain(item.category),
+            city: CityMapper.toDomain(item.city),
         }));
     }
 
@@ -60,12 +62,13 @@ export class ProformaRequestsPrismaRepository implements IProformaRequestsReposi
     async findAllByClientId(clientId: string, status: StatusRequestVO): Promise<ProformaRequestWithRelations[]> {
         const proformaRequests = await this.db.proformaRequest.findMany({
             where: { AND: [{ clientId }, { status: status.getValue() }] },
-            include: { skills: true, category: true },
+            include: { skills: true, category: true, city: true },
         });
         return proformaRequests.map((item) => ({
             proformaRequest: ProformaRequestsMapper.toDomain(item),
             skills: item.skills.map((s) => SkillMapper.toDomain(s)),
             category: CategoryMapper.toDomain(item.category),
+            city: CityMapper.toDomain(item.city),
         }));
     }
 }
